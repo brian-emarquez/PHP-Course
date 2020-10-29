a84f682f3454f0b75b936e8bb1eb43f5e097f683
BrianMarquez3 <brian3marquez@gmail.com>
2020-10-29 14:02:31 -0500

    feat array
    Array
    
    resolves:
    see also:

---
+++ Array/array.php
@@ -0,0 +1,10 @@
+<!-- Array - Vecor -->
+
+<?php
+
+//Arreglo , vectores , array
+$dias = array('lunes <br>', 'martes <br>', 'miercoles <br>', 'jueves <br>');
+var_dump($dias); //var_dump identfica el tiempo de variable
+print_r($dias) // poscion del array
+
+?>
