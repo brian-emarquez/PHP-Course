71773548d212466bd8dca9bffe00ce1b67e17a7c
BrianMarquez3 <brian3marquez@gmail.com>
2020-10-29 08:39:28 -0500

    feat switch case complete
    switch case complete
    
    resolves:
    see also:

--- Switch Case/switchcase.php
+++ Switch Case/switchcase.php
@@ -1,7 +1,24 @@
 <?php
 
+$curso = 'php';
 
+switch ($curso) {
+    case 'php':
+        echo "Lunes y Martes";
+        break;
 
+    case 'java':
+        echo "miercoles";
+        break;
+
+    case 'python':
+        echo "jueves";
+        break;
+    
+    default: /*si no existe ninguna de la conciones*/
+        echo "No se encuntra en la opciones";# code...
+        break;
+}
 
 
 
