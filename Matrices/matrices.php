742590ffdcbdbf179e315396056089b261323607
BrianMarquez3 <brian3marquez@gmail.com>
2020-10-29 16:27:46 -0500

    feat PHP MATRICES complete
    PHP MATRICES complete
    
    resolves:
    see also:

--- Matrices/matrices.php
+++ Matrices/matrices.php
@@ -1,8 +1,14 @@
-<!-- Matrices -->
+<!-- Matrices, arreglos multidimencionales-->
+<!-- 3X5 -->
 
 <?php
-// Matrices
 
+$matriz = array(
+    array(1,2,3,4,5),
+    array(6,7,8,9,10),
+    array(11,12,13,14,15)
 
+);
+echo $matriz[1][1];
 
 ?>
