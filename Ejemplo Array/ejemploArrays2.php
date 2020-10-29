8f9cbf5fe245ef508cef175f6cad0c7b95acdea4
BrianMarquez3 <brian3marquez@gmail.com>
2020-10-29 14:47:51 -0500

    feat asociativo ejecrcicios 2 complete
    Array ejercicios 2 complete
    
    resolves:
    see also:

--- Ejemplo Array/ejemploArrays2.php
+++ Ejemplo Array/ejemploArrays2.php
@@ -3,8 +3,22 @@
 <?php
 // Ejercio con Array 2
 
-$cursos = array ('python', 'java', 'html', 'html', 'css');
-echo $cursos[3];
+$semana[0] = ('lunes<br>');
+$semana[1] = ('Martes<br>');
+$semana[2] = ('miercoles<br>');
+$semana[3] = ('jueves');
+$semana[4] = ('viernes<br>');
+$semana[5] = ('sabado<br>');
+$semana[6] = ('domiengo<br>');
 
+echo 'cantidad de elemento :'.count($semana);
+echo '<br>';
+
+for ($i=0; $i<count($semana) ;$i++ ){
+    //echo $semana[$i];
+    if($semana[$i] == 'jueves'){
+    echo "curso de php";
+    }
+}
 
 ?>
