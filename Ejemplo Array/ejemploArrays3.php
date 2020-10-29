85adccea1568088547f82b5495d599ec5e9423fd
BrianMarquez3 <brian3marquez@gmail.com>
2020-10-29 14:58:06 -0500

    feat asociativo ejecrcicios 3 complete
    Array ejercicios 3 complete
    
    resolves:
    see also:

--- Ejemplo Array/ejemploArrays3.php
+++ Ejemplo Array/ejemploArrays3.php
@@ -3,4 +3,20 @@
 <?php
 // Ejercio con Array 3
 
+$amigos = array('jose', 'maria', 'raul', 'sara', 'pedro', 'camila', 'david');
+
+$amigos[5] = "brian"; 
+$posicion = count($amigos) -2;
+echo $amigos[$posicion];
+echo "<br>";
+
+// Ordenas lo elementos de manera alfavetico
+//asort
+//rsort
+
+asort($amigos);
+print_r($amigos);
+
+
+
 ?>
