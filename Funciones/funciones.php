1ca76bf61701b1c670e60ddb963a6b775bb571f2
BrianMarquez3 <brian3marquez@gmail.com>
2020-10-29 17:03:35 -0500

    feat FUNciones 2
    FUNCIONES 2
    
    resolves:
    see also:

--- Funciones/funciones.php
+++ Funciones/funciones.php
@@ -1,9 +1,13 @@
 <!-- FUNCIONES -->
 <?php
 
+function estudiante($nombre)
+{
+    echo "nuevo estudiante $nombre<br>";
+}
 
-
-
-
+estudiante('brian');
+estudiante('quennie');
+estudiante('maria');
 
 ?>
---
+++ Funciones/funciones2.php
@@ -0,0 +1,12 @@
+<!-- FUNCIONES 2 -->
+<?php
+
+function estudiante($nombre = 'victor', $apellido = 'cruz', $edad = '24') 
+{
+    echo "El estudiante de nombre: $nombre con el apellido : $apellido con la edad de : $edad<br>";
+}
+
+#estudiante()
+estudiante('brian', 'marquez', 26);
+
+?>
