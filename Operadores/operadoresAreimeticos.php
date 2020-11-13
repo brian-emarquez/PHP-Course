393bc79ad79469f80834895fa3a600e419298b87
BrianMarquez3 <brian3marquez@gmail.com>
2020-11-13 17:32:27 -0500

    feat php course operadores start
    php course - Operadores start
    
    resolves:
    see also:

---
+++ Operadores/operadoresAreimeticos.php
@@ -0,0 +1,22 @@
+<!-- Operadores Arimeticos -->
+
+<?php 
+
+$numero = 10;
+$rta = $numero +2;
+
+$rta1 = $numero -2;
+
+$rta2 = $numero /2;
+
+//modulo
+$rta3 = $numero %2;
+
+
+var_dump($rta);
+var_dump($rta1);
+var_dump($rta2);
+var_dump($rta3);
+
+
+?>
