39167f213b981b6bfa53a0bfb3572fc8e03c4d0d
BrianMarquez3 <brian3marquez@gmail.com>
2020-11-13 17:52:48 -0500

    feat php course operadores incremento
    php course - Operadores incremento
    
    resolves:
    see also:

--- Operadores/incremento.php
+++ Operadores/incremento.php
@@ -1,1 +1,16 @@
 <!-- Operadores de Incremento -->
+
+<?php
+
+$numero = 10;
+
+echo "Numero tiene calor : " .$numero;
+
+$numero++;
+
+echo "<br /> Despues del ++ El numero tiene como valor: ".$numero;
+echo"<hr />";
+echo "La variable numero finalizo con <br> ";
+var_dump($numero);
+
+?>
