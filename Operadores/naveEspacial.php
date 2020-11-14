86ae4b49436d0cfbfb1cf63ac08c4fc22e50474e
BrianMarquez3 <brian3marquez@gmail.com>
2020-11-13 23:06:09 -0500

    feat php course operadores update6
    php course - Operadores update6
    
    resolves:
    see also:

--- Operadores/naveEspacial.php
+++ Operadores/naveEspacial.php
@@ -1,9 +1,14 @@
 <!-- Operador "Nave Espacal -->
 
+<!-- nos da 3 tipos de resultados -->
+ <!-- -1 -->
+ <!-- 0 -->
+ <!-- 1 -->
+
 <?php
 
-$numero = 10;
-$rta = $numero <=> 10;
+$numero = 19;
+$rta = $numero <=> 19;
 
 var_dump($rta);
 
