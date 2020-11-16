6178c8529b4f390d1387667a9db851facd17916d
BrianMarquez3 <brian3marquez@gmail.com>
2020-11-16 18:47:21 -0500

    feat php course selector-case UPDATE
    php course - selector-case UPDATE
    
    resolves:
    see also:

--- Selector-Case/selector.php
+++ Selector-Case/selector.php
@@ -4,19 +4,23 @@
 <?php 
 
 
-$nivel = "usuario";
+$nivel = "redactor";
 
 
 switch($nivel){
 
     case 'administrador': #acciones
         echo "Hola Amidn";
+    echo "<hr />";
 
     case 'redactor': #acciones 
         echo "El redactor puede publicar";
+    echo "<hr />";
 
     case 'usuario': // acciones
         echo "El Usuario Comun solo puede comentar/likear";
+    echo "<hr />";
+
 
 
 
