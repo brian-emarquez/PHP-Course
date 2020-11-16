31d52fccedb61ec98c24fc3814fcfa8aba38054d
BrianMarquez3 <brian3marquez@gmail.com>
2020-11-16 17:06:20 -0500

    feat php course triple igual complete
    php course - triple igual complete
    
    resolves:
    see also:

--- Operadores/tipleigual.php
+++ Operadores/tipleigual.php
@@ -1,16 +1,19 @@
+<!-- TIPLE IGUAL -->
+
 <?php
 
-$numero = 5;
-$numero = $numero ===5;
-$srta2 = $numero ==5;
-
+$numero = "5";
+$rta3 = $numero === 5;
+$rta2 = $numero == 5;
 $rta1 = $numero = 5;
 
-echo "con un solo ugual... guarda 5 dentro de $numero  adentro de $rta1;
-var_dumb($rta1);
-
-
-
-
+#echo 'con un solo ugual... guarda 5 dentro de $numero  adentro de $rta1';
+var_dump($rta1);
+echo "<hr />";
+#echo 'Comparacion con dos igual... guarda true o false , segun sen el mismo valor o no';
+var_dump($rta2); # no fierencia el tipo de dato
+echo "<hr />";
+#echo 'Comparacion con 3 iguales.. guarda true o false';
+var_dump($rta3); # si deferencia el tipo de dato
 
 ?>
