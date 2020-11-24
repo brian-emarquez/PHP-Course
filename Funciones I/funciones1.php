bccf9827474e0c7223a87639bcb8bb16fe2cd3fb
BrianMarquez3 <brian3marquez@gmail.com>
2020-11-24 15:16:44 -0500

    feat complete Funciones
    complete funciones
    
    resolves:
    see also:

--- Funciones I/funciones1.php
+++ Funciones I/funciones1.php
@@ -2,7 +2,16 @@
 
 <?php
 
+$nombre = "Brian";
+$email = "brian@mail.com";
 
+function saludar( ){
+    echo "Hola Usuario bienvenido al sistema";
+}
+
+saludar();
 
 
 ?>
+
+<!-- no puede existir funciones con el mismo nombre -->
