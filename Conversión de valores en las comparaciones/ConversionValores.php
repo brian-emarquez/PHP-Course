8219f01c76643a4478fdd1da04bf07cb8569aa7a
BrianMarquez3 <brian3marquez@gmail.com>
2020-11-24 14:30:07 -0500

    feat onversion de Valores
    onversion de Valores
    
    RESOLVER:
    see algo:

---
+++ Conversión de valores en las comparaciones/ConversionValores.php
@@ -0,0 +1,15 @@
+
+<!-- ConversionValores  -->
+
+<?php
+
+$nommbre = "juan";
+
+if($nommbre = "Ryan"){
+    echo "Bienvenido profesor";
+}
+
+echo "<hr />";
+var_dump($nommbre);
+
+?>
---
+++ Conversión de valores en las comparaciones/ConversionValores2.php
@@ -0,0 +1,15 @@
+
+<!-- Conversion de Valores 2-->
+
+<?php
+
+$valor = "juan";
+
+var_dump  ($valor);
+
+if ($valor):
+    echo "$valor es verdadero";
+else:
+    echo "$valor es falso";
+endif;
+?>
