6919e74f9e9d46d58aabe36eb62e7d7c2f57cc73
BrianMarquez3 <brian3marquez@gmail.com>
2020-12-15 17:24:31 -0500

    feat Manejo de Fechas: Obtener diferencia entre fechas 2
    Manejo de Fechas: Obtener diferencia entre fechas 2
    
    resolvesr: #
    se also: #

--- Manejo de Fechas Obtener diferencia entre fechas/ManejodeFechasObtenerdiferenciaentrefechas2.php
+++ Manejo de Fechas Obtener diferencia entre fechas/ManejodeFechasObtenerdiferenciaentrefechas2.php
@@ -6,6 +6,23 @@
 <?php
 # Dia de las pascua
 
+$anio = 2010;
+
+$pascuas_epoch = easter_date($anio);
+$pascuas_spa = date("d-m-Y", $pascuas_epoch );
+
+$jueves_epoch = strtotime( '-3 day', $pascuas_epoch );
+$viernes_epoch = strtotime( '-2 day', $pascuas_epoch ); 
+
+$jueves_spa = date( "d-m-Y", $jueves_epoch );
+$viernes_spa = date( "d-m-Y", $viernes_epoch );
+
+//echo $pascuas_epoch;
+echo "Domiengo de pascuas ($anio): $pascuas_spa";
+echo '<hr />';
+echo "Jueves de pascuas ($anio): $jueves_spa";
+echo '<hr />';
+echo "Viernes de pascuas ($anio): $viernes_spa";
 
 
 ?>
