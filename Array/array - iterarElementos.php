ca3c35cde748009a476e20d3d5995585222ec1a0
BrianMarquez3 <brian3marquez@gmail.com>
2020-12-16 14:07:49 -0500

    feat Array iterar elementos
    Array iterar elementos
    
    also:
    see:

---
+++ Array/array - iterarElementos.php
@@ -0,0 +1,15 @@
+<!-- ######################################################## -->
+<!-- Array - Iterar Elementos  -->
+<!-- ######################################################## -->
+
+<?php
+
+$tecnologias = [ "php", "mysql", "htmp", "css " ]; // 
+
+$tecnologias[4] = 'linux'; // agregar un elemento.
+array_push($tecnologias, 'unix', 'data', 'update', 'flow'); // agregar un elemento. en grupo
+
+$tecnologias[] = 'javascript';
+$tecnologias[] = 'bash';
+
+?>
