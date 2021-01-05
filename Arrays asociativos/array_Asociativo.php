847739e4a288d20a307e254a908f5e87e09895eb
Brian Enrique Marquez Inca Roca <brrian3marquez@gmail.com>
2021-01-05 16:50:01 -0500

    feat PHP Course Array Asociativo
    PHP Course Array Asociativo
    
    resolver: #
    se also: #

---
+++ Arrays asociativos/array_Asociativo.php
@@ -0,0 +1,64 @@
+<?php 
+
+$select = array( );
+$select['name'] = 'motivo';
+$select['required'] = false;
+$select['tabindex'] = 2;
+
+
+//var_dump($select);
+
+$elementos = array( 
+	/*0*/ array( 
+		'etiqueta' => 'input',
+		'type'  => 'text',
+		'name'  => 'nombre',
+		'placeholder' =>  'Tu nombre completo',
+		'texto'  => 'Tu nombre'
+	),
+	/*1*/ array( 
+		'etiqueta' => 'input',
+		'type' => 'email',
+		'name' => 'correo',
+		'placeholder' => 'usuario@email.com',
+		'texto' => 'Tu correo'
+	),
+	/*2*/ array( 
+		'etiqueta' => 'select',
+		'name' => 'motivo',
+		'texto' => 'Motivo del mensaje',
+		'opciones' => array( 'contacto', 'sugerencia', 'reclamo' )
+	),
+	array( 
+		'etiqueta' => 'input',
+		'type' => 'checkbox',
+		'name' => 'item',
+		'texto' => 'Acepto los terminos y condiciones'
+	)
+	
+);
+for( $i = 0; $i < count($elementos); $i++ ){
+	//echo $elementos[$i][0];
+	
+	echo '<p>'.$elementos[$i]['texto'].'</p>';
+	
+	if( $elementos[$i]['etiqueta'] == 'input' ){
+		echo '<input type="'.$elementos[$i]['type'].'" name="'.$elementos[$i]['name'].'" />';
+	}else if( $elementos[$i]['etiqueta'] == 'select' ){
+		echo '<select name="'.$elementos[$i]['name'].'">';
+		for( $x = 0; $x < count ($elementos[$i]['opciones'] ); $x++ ){
+			echo '<option>';
+			echo $elementos[$i]['opciones'][$x];
+			echo '</option>';
+			
+		}
+		echo '</select>';
+	}
+	
+	echo '<hr />';
+}
+
+
+
+
+?>
---
+++ Arrays asociativos/readme.md
@@ -0,0 +1,12 @@
+## Array Asociativos
+
+* Un Array asociativo es el que en lugar de indices basados en numeros los tiene con cadenas de texto
+
+* de esa manera los indices estará entre comillas.
+
+* Esto permite salir de las abstracion de lo indices 0,1,2 y facilita identificar la informacion que estamos manejando
+
+## Instanciar un array asociativo
+
+* En PHP tenemos dos maneras de crear un array con indices asociativos.
+* la Primera opcion es crear el array vacio y luego ir creado indice por indice (de la misma manera asignabamos los numericos).
