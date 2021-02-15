466190efcd3f4f4e09ed1957e7eb536e301bc838
BrianMarquez3 <brian3marquez@gmail.com>
2021-02-15 18:50:05 -0500

    feat Leer y escribir archivos en el servidor COMPLETE
    Leer y escribir archivos en el servidor COMPLETE
    resolves: #
    see also: #

--- Leer y escribir archivos en el servidor/admin/acciones/editar_texto.php
+++ Leer y escribir archivos en el servidor/admin/acciones/editar_texto.php
@@ -3,7 +3,11 @@
 $contenido = $_POST['contenido'];
 $archivo = $_POST['quien'];
 
-file_put_contents("../../extras/textos/institucional/$archivo.txt", $contenido);
+file_put_contents(
+    "../../extras/textos/institucional/$archivo.txt",
+    $contenido
+    /*FILE_APPEND /*AGREGA EL NUEvo contenido al final de archvo*/
+);
 
 header("Localtion: ../index.php");
 
