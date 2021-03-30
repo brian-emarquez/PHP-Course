7d7e4243e7d9f4d43fb125664c36bd0e0d78a0b6
BrianMarquez3 <brian3marquez@gmail.com>
2021-03-29 19:43:48 -0500

    Update

--- Libreria GDLibrary Redimensionar imagenes/admin/acciones/estaticos_borrar.php
+++
@@ -1,5 +0,0 @@
-<?php 
-$nombre = $_GET['archivo'];
-unlink( "../../extras/textos/institucional/$nombre.txt" );
-header("Location: ../index.php?cat=estaticos");
-?>
--- Libreria GDLibrary Redimensionar imagenes/contenidos/registro.php
+++
@@ -1,9 +0,0 @@
-
-		<h1>Registrate</h1>
-		<p class="info">Completa el siguiente formulario para crear tu cuenta en este blog</p>
-		<form>
-			<div><span>Tu Email </span><input type="email" ></div>
-			<div><span>Tu Clave </span><input type="password" ></div>
-			<div><input class="aligned" type="submit" value="Registro" /></div>
-		</form>
-	</main>
