c8d471c0e56ec51d1c60a37767733b1847f0d7fe
BrianMarquez3 <brian3marquez@gmail.com>
2021-04-28 12:50:28 -0500

    Espresiones Regualres II

--- 046 Espresiones Regulares_II/er2.php
+++ 046 Espresiones Regulares_II/er2.php
@@ -1,34 +1,32 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="es">
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+	<meta charset="UTF-8" />
+	<meta name="viewport" content="width=device-width, initial-scale=1" />
+	<title>New document</title>
 </head>
 <body>
-    <form method="get" action="er2.php">
-        <p> Registre su automovil ingresando su patente (AAA000 / AA000AA) </p>
-        <input type="text" placeholder="Numero de patente" name="patente" autocomplete="off" />
-    <input type="submit" value="enviar" />
-    </form>
-    <?php
-    if(isset($_GET['patente'])){
-        $p = $_GET['patente'];
-        $er = "/...../";
-
-        echo "Patente recibida: $p<br />";
-        $rta = preg_match($er, $p, $xq);
-
-        if($rta){
-            echo "La patente $p coincide con la expresion regular $er<br />";
-            echo "motivos: ";
-            var_dump($xq);
-        }else{
-            echo "La patente $p NO coincide con el formato $er<br />";
-        }
-    }
-    ?>
-
+	<form method="get" action="er2.php">
+		<p>Registre su automovil ingresando su patente (AAA000 / AA000AA)</p>
+		<input type="text" placeholder="Numero de patente" name="patente" autocomplete="off" />
+		<input type="submit" value="Enviar" />
+	</form>
+	<?php 
+	if( isset( $_GET['patente'] ) ){
+		$p = $_GET['patente'];
+		$er = "/([a-z][A-Z][a-z][0-9][0-9][0-9]|[a-z][a-z]([0-9][0-9][0-9])[a-z][a-z])/i";
+		
+		echo "Patente recibida: $p<br />";
+		$rta = preg_match( $er, $p, $xq );
+		if( $rta ){
+			echo "La patente $p coincide con la expresión regular $er<br />";
+			echo "motivos:";
+			var_dump($xq);
+		}else{
+			echo "La patente $p <br />NO coincide con el formato esperado $er<br />";
+			
+		}
+	}
+	?>
 </body>
 </html>
